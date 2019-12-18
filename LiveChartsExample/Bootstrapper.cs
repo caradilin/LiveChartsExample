c4576991a030e57a26293bcf6b61108a4ba2a158
caradilin <2212639304@qq.com>
2019-12-18 09:38:08 +0800

    框架

---
+++ LiveChartsExample/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsExample.ViewModel;
+using MahApps.Metro.Controls.Dialogs;
+using Stylet;
+using StyletIoC;
+
+namespace LiveChartsExample
+{
+    public class Bootstrapper : Bootstrapper<ShellViewModel>
+    {
+        protected override void ConfigureIoC(IStyletIoCBuilder builder)
+        {
+            // Configure the IoC container in here
+        }
+
+        protected override void Configure()
+        {
+            // Perform any other configuration before the application starts
+        }
+    }
+}
---
+++ LiveChartsExample/ViewModel/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveChartsExample.ViewModel
+{
+    public class ViewModelBase : Screen
+    {
+        /// <summary>
+        /// Logger
+        /// </summary>
+        protected static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+    }
+}
